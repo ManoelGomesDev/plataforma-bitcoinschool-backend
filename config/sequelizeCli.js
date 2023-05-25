a297b2b0066c8a62dba7990175d73aef6c735c74
Manoel Lucio <manoellucio@Manoels-MacBook-Air.local>
2023-05-25 20:17:08 -0300

    sequelize configuration

---
+++ config/sequelizeCli.js
@@ -0,0 +1,12 @@
+// config/sequelizeCli.js
+
+module.exports = {
+    development: {
+        dialect: 'postgres',
+        host: 'localhost',
+        port: '5432',
+        database: 'bitcoinschool_development',
+        username: 'bitcoinschool',
+        password: 'bitcoinschool'
+    }
+}
