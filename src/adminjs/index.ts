78a7fef66a1cd3f9352d1720fc5f5826cd2525cb
Manoel Lucio <manoellucio@Manoels-MacBook-Air.local>
2023-06-03 00:36:25 -0300

    created setup of adm dashboard

--- src/adminjs/index.ts
+++ src/adminjs/index.ts
@@ -4,38 +4,29 @@ import AdminJs from 'adminjs'
 import AdminJsExpress from '@adminjs/express'
 import AdminJsSequelize from '@adminjs/sequelize'
 
+
+
 import { sequelize } from '../database'
 import { adminJsResources } from "./resources";
+import { Category, Course, Episode, User } from '../models';
+
+import { locale } from './locale'
+import { dashboardOptions } from './dashboard'
+import { brandingOptions } from './branding'
+import { authtenticationOptions } from './authentication'
 
 AdminJs.registerAdapter(AdminJsSequelize)
 
 export const adminJs = new AdminJs({
     databases: [sequelize],
     rootPath: '/admin',
+    dashboard: dashboardOptions,
+    locale: locale,
     resources: adminJsResources,
-    branding: {
-        companyName: 'BitcoinSchool',
-        logo: '/bitcoinschool.svg',
-        theme: {
-            colors: {
-
-
-                primary100: '#f7d01d',
-                primary80: '#f9dc3a',
-                primary60: '#fbe856',
-                primary40: '#fdf373',
-                primary20: '#ffff8f',
-                grey100: '#151515',
-                grey80: '#333333',
-                grey60: '#4d4d4d',
-                grey40: '#666666',
-                grey20: '#dddddd',
-                filterBg: '#333333',
-                accent: '#151515',
-                hoverBg: '#151515',
-            }
-        }
-    }
+    branding: brandingOptions,
 })
 
-export const adminJsRouter = AdminJsExpress.buildRouter(adminJs)
+export const adminJsRouter = AdminJsExpress.buildAuthenticatedRouter(adminJs, authtenticationOptions , null, {
+    resave: false,
+    saveUninitialized: false
+})
--- src/adminjs/resources/index.ts
+++ src/adminjs/resources/index.ts
@@ -1,10 +1,11 @@
 // src/adminjs/resources.ts
 
 import { ResourceWithOptions } from "adminjs";
-import { Category, Course, Episode } from "../../models";
+import { Category, Course, Episode, User } from "../../models";
 import { categoryResourceOptions } from "./category";
 import { courseResourceFeatures, courseResourceOptions } from "./course";
 import { episodeResourceFeatures, episodeResourceOptions } from "./episode";
+import { userResourceOptions } from "./user";
 
 
 export const adminJsResources: ResourceWithOptions[] = [
@@ -12,6 +13,10 @@ export const adminJsResources: ResourceWithOptions[] = [
         resource: Category,
         options: categoryResourceOptions
     },
+    {
+        resource: User,
+        options: userResourceOptions
+      },
     {
         resource: Course,
         options: courseResourceOptions,
